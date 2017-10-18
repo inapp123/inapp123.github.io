a773914eb07670ad7e6940925f15deb2ee55cc24
inapp123 <1036015366@qq.com>
2017-10-18 17:31:16 +0800

    Create getonslibs.sh

---
+++ getonslibs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+mkdir onslib
+cd onslib
+wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
+git clone https://github.com/oneman/libflac.git
+wget http://download.savannah.gnu.org/releases/freetype/freetype-2.8.1.tar.gz
+tar xzvf freetype-2.8.1.tar.gz
+#libjpeg? https://sourceforge.net/projects/libjpeg/files/latest/download?source=files
+#libmikmod
+#libmodplug
+#libogg
+#libpng16
+#libtiff
+#libvorlis
+#libvorlisfile
