4d5b141ba3a9045a6ec3b75d43fbe483947b66f0
inapp123 <1036015366@qq.com>
2017-10-19 11:57:53 +0800

    神TM继续更新

--- getonslibs.sh
+++ getonslibs.sh
@@ -5,11 +5,11 @@ wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
 git clone https://github.com/oneman/libflac.git
 wget http://download.savannah.gnu.org/releases/freetype/freetype-2.8.1.tar.gz
 tar xzvf freetype-2.8.1.tar.gz
-#libjpeg? https://sourceforge.net/projects/libjpeg/files/latest/download?source=files
-#libmikmod
-#libmodplug
-#libogg
-#libpng16
-#libtiff
-#libvorlis
+git clone https://github.com/thorfdbg/libjpeg.git
+hg clone http://hg.code.sf.net/p/mikmod/mercurial mikmod-mercurial
+git clone https://github.com/solarus-games/libmodplug.git
+wget http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+wget https://ftp-osl.osuosl.org/pub/libpng/src/libpng16/libpng-1.6.34.tar.gz
+wget http://dl.maptools.org/dl/libtiff/tiff-3.8.2.tar.gz
+wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
 #libvorlisfile
