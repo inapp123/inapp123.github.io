11f0681cfab8a70717f8f963169e5f5f2d0409cc
inapp123 <1036015366@qq.com>
2017-10-20 08:37:05 +0800

    Update getonslibs.sh

--- getonslibs.sh
+++ getonslibs.sh
@@ -2,7 +2,7 @@
 mkdir onslib
 cd onslib
 wget http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
-git clone https://github.com/oneman/libflac.git
+wget https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.2.tar.xz
 wget http://download.savannah.gnu.org/releases/freetype/freetype-2.8.1.tar.gz
 tar xzvf freetype-2.8.1.tar.gz
 git clone https://github.com/thorfdbg/libjpeg.git
@@ -12,4 +12,9 @@ wget http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
 wget https://ftp-osl.osuosl.org/pub/libpng/src/libpng16/libpng-1.6.34.tar.gz
 wget http://dl.maptools.org/dl/libtiff/tiff-3.8.2.tar.gz
 wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
-#libvorlisfile
+git clone https://github.com/webmproject/libwebp.git
+git clone https://github.com/LuaDist/lua.git
+hg clone http://hg.libsdl.org/SDL
+hg clone http://hg.libsdl.org/SDL_image/
+hg clone http://hg.libsdl.org/SDL_mixer/
+hg clone http://hg.libsdl.org/SDL_ttf/
